0c728de9cca8051b7228701239795904d0a5c874
chimsara <ericanwoga@gmail.com>
2022-12-09 16:04:36 -0500

    Set up project

---
+++ README.md
@@ -0,0 +1,16 @@
+![banner](./images/flag-banner.png)
+
+# **Study Abroad Application**
+Made by Erica Nwoga
+
+
+## **Description**
+Students apply for a study abroad program by entering their information. The database stores this data, and displays other students in the program.
+
+## **Tech Stack**
+JavaScript, NodeJS, ExpressJS, MongoDB, HTML, CSS
+
+## **API**
+[World Geo Data API](https://rapidapi.com/natkapral/api/world-geo-data)
+
+## **Demo Video**
---
+++ studyAbroad.js
@@ -0,0 +1,31 @@
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, './.env') })  
+const express = require("express"); //express module which is our server
+const app = express();  // app creates an application  to set up our server
+const portNumber = 3000;
+const prompt = "Stop to shutdown the server: ";
+app.listen(portNumber); //app will run on this server  
+
+const bodyParser = require("body-parser"); // handles post paramer
+
+const userName = process.env.MONGO_DB_USERNAME;
+const password = process.env.MONGO_DB_PASSWORD;
+
+// Our database and collection 
+const databaseAndCollection = {db: "studyAbroad_DB", collection:"students"};
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+//local directory where templates will reside
+app.set("views", path.resolve(__dirname, "templates"));
+app.set("view engine", "ejs");
+console.log(`Web server is running at http://localhost:${portNumber}`);
+process.stdout.write(prompt);
+process.stdin.setEncoding("utf8"); //encoding
+
+
+app.use(bodyParser.urlencoded({extended:false})); //allows parsing
+
+//Our home page
+app.get("/", (req, res) => {
+    response.render("index");
+});
