994a41345f81a244388c0ddbb580da8e58fd4c79
chimsara <ericanwoga@gmail.com>
2022-12-09 19:19:57 -0500

    Added images for cities

--- studyAbroad.js
+++ studyAbroad.js
@@ -3,13 +3,14 @@ const favicon = require('serve-favicon'); //favicon
 const express = require("express"); //express module which is our server
 require("dotenv").config({ path: path.resolve(__dirname, './.env') })  
 const app = express();  // app creates an application  to set up our server
-const portNumber = 3000;
+const portNumber = 5500;
 const prompt = "Stop to shutdown the server: ";
 app.use(favicon(path.join(__dirname, 'favicon', 'favicon.ico')));
 app.listen(portNumber); //app will run on this server  
-
-
+app.set("views", path.resolve(__dirname, "views")); //local directory where templates will reside
+app.set("view engine", "ejs"); //allows us to use ejs
 const bodyParser = require("body-parser"); // handles post paramer
+app.use(bodyParser.urlencoded({extended:false})); //allows middleware body parsing
 
 const userName = process.env.MONGO_DB_USERNAME;
 const password = process.env.MONGO_DB_PASSWORD;
@@ -18,17 +19,20 @@ const password = process.env.MONGO_DB_PASSWORD;
 const databaseAndCollection = {db: "studyAbroad_DB", collection:"students"};
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-//local directory where templates will reside
-app.set("views", path.resolve(__dirname, "templates"));
-app.set("view engine", "ejs");
+
 console.log(`Web server is running at http://localhost:${portNumber}`);
 process.stdout.write(prompt);
 process.stdin.setEncoding("utf8"); //encoding
 
 
-app.use(bodyParser.urlencoded({extended:false})); //allows parsing
 
 //Our home page
 app.get("/", (req, res) => {
     res.render("index");
+});
+
+
+//show application form
+app.get("/apply", (req, res) => { 
+    res.render("apply");
 });
